901add9f4ed21713336a4ceecbd002f902218156
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-05-20 14:10:01 +0700

    Move defines and structures description to header file

--- rtree.c
+++ rtree.c
@@ -4,53 +4,6 @@
 #include <stdbool.h>
 #include "rtree.h"
 
-#define DATATYPE void * 
-#define NUMTYPE double
-#define DIMS 2
-#define MAX_ENTRIES 64
-#define MIN_ENTRIES_PERCENTAGE 10
-#define FAST_CHOOSER 2  // 0 - off , 1 - fast, 2 - faster
-
-#define panic(_msg_) { \
-    fprintf(stderr, "panic: %s (%s:%d)\n", (_msg_), __FILE__, __LINE__); \
-    exit(1); \
-}
-
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
-
-#define MIN_ENTRIES ((MAX_ENTRIES) * (MIN_ENTRIES_PERCENTAGE) / 100 + 1)
-
-enum kind {
-    LEAF = 1,
-    BRANCH = 2,
-};
-
-struct rect {
-    NUMTYPE min[DIMS];
-    NUMTYPE max[DIMS];
-};
-
-struct item {
-    DATATYPE data;
-};
-
-struct node {
-    enum kind kind;     // LEAF or BRANCH
-    int count;          // number of rects
-    struct rect rects[MAX_ENTRIES];
-    union { struct node *children[MAX_ENTRIES]; struct item items[MAX_ENTRIES]; };
-};
-
-struct rtree {
-    size_t count;
-    int height;
-    struct rect rect;
-    struct node *root; 
-    void *(*malloc)(size_t);
-    void (*free)(void *);
-};
-
 struct node *node_new(struct rtree *tr, enum kind kind) {
     struct node *node = (struct node *)tr->malloc(sizeof(struct node));
     memset(node, 0, sizeof(struct node));
@@ -581,10 +534,8 @@ void rtree_delete(struct rtree *tr, const NUMTYPE *min, const NUMTYPE *max, cons
             tr->rect = node_rect_calc(tr->root);
         }
     }
-    
 }
 
-// checker
 void node_check_order(struct node *node) {
     for (int i = 1; i < node->count; i++) {
         if (node->rects[i].min[0] < node->rects[i-1].min[0]) {
--- rtree.h
+++ rtree.h
@@ -3,23 +3,53 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define DATATYPE void * 
+#define NUMTYPE double
+#define DIMS 2
+#define MAX_ENTRIES 64
+#define MIN_ENTRIES_PERCENTAGE 10
+#define FAST_CHOOSER 2  // 0 - off , 1 - fast, 2 - faster
+#define panic(_msg_) { \
+    fprintf(stderr, "panic: %s (%s:%d)\n", (_msg_), __FILE__, __LINE__); \
+    exit(1); \
+}
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN_ENTRIES ((MAX_ENTRIES) * (MIN_ENTRIES_PERCENTAGE) / 100 + 1)
+
+enum kind {
+    LEAF = 1,
+    BRANCH = 2,
+};
+
+struct rect {
+    NUMTYPE min[DIMS];
+    NUMTYPE max[DIMS];
+};
+
+struct item {
+    DATATYPE data;
+};
+
+struct node {
+    enum kind kind;     // LEAF or BRANCH
+    int count;          // number of rects
+    struct rect rects[MAX_ENTRIES];
+    union { struct node *children[MAX_ENTRIES]; struct item items[MAX_ENTRIES]; };
+};
+
+struct rtree {
+    size_t count;
+    int height;
+    struct rect rect;
+    struct node *root; 
+    void *(*malloc)(size_t);
+    void (*free)(void *);
+};
+
 struct rtree *rtree_new();
-
-struct rtree *rtree_new_with_allocator(void *(*malloc)(size_t), void (*free)(void*));
-
 bool rtree_insert(struct rtree *tr, const double *min, const double *max, const void *data);
-
 void rtree_free(struct rtree *tr);
-
-void rtree_search(struct rtree *tr, const double *min, const double *max,
-    bool (*iter)(const double *min, const double *max, const void *data, void *udata), 
-    void *udata);
-
+void rtree_search(struct rtree *tr, const double *min, const double *max, bool (*iter)(const double *min, const double *max, const void *data, void *udata), void *udata);
 size_t rtree_count(struct rtree *tr);
-
 void rtree_delete(struct rtree *tr, const double *min, const double *max, const void *data);
-
-void rtree_delete_with_comparator(struct rtree *tr, const double *min, 
-    const double *max, const void *data,
-    int (*compare)(const void *a, const void *b, void *udata),
-    void *udata);
